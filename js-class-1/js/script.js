af8bdfe855ccfb158af705f288dd9b9d166dcbd8
unknown <dibyathapa223@gmail.com>
2026-03-11 11:23:48 +0545

    java script

---
+++ js-class-1/js/script.js
@@ -0,0 +1,22 @@
+console.log("hello king's college");
+var a = 5;
+var b= 6;
+var sum= a+b;
+console.log("sum of a and b is",sum)
+
+console.log("hello king's college");
+var a = 148;
+var b= 3;
+var remainder= a%b;
+console.log("remainder of a and b is",remainder)
+
+var a3=5;
+if(true){
+    console.log(a3);
+    var a1=5;
+    console.log(a3);
+
+}
+console.log("blocked scope",a3);
+
+   
